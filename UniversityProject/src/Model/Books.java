b08b732d18e115d936f5a771947435b1bb9a10c5
Daniel Kostov <45979039+DdanielRrKk@users.noreply.github.com>
2020-11-03 18:49:40 +0200

    Updating BooksModel

--- UniversityProject/src/Model/Books.java
+++ UniversityProject/src/Model/Books.java
@@ -7,77 +7,41 @@ public class Books {
 	private String Author;
 	private String Genre;
 	private int Condition;
-	private boolean bAvailable;
+	private boolean Available;
 	
-	public Books() 
-	{
-	}
+	public Books() {	}
 	
-	public Books(String title, String author, String genre, int condition, boolean avaliable)
-	{
+	public Books(String title, String author, String genre, int condition, boolean avaliable){
 		this.Title = title;
 		this.Author = author;
 		this.Genre = genre;
 		this.Condition = condition;
-		this.bAvailable = avaliable;
+		this.Available = avaliable;
 	}
 	
-	public Books(int id, String title, String author, String genre, int condition, boolean avaliable)
-	{
+	public Books(int id, String title, String author, String genre, int condition, boolean avaliable){
 		this.ID = id;
 		this.Title = title;
 		this.Author = author;
 		this.Genre = genre;
 		this.Condition = condition;
-		this.bAvailable = avaliable;
+		this.Available = avaliable;
 	}
 	
-	public int getID() {
-		return ID;
-	}
+	// setters
+	public void setID(int id) { this.ID = id; }
+	public void setTitle(String title) { Title = title; }
+	public void setAuthor(String author) { Author = author; }
+	public void setGenre(String genre) { Genre = genre; }
+	public void setCondition(int condition) { this.Condition = condition; }
+	public void setAvailable(boolean available) { this.Available = available; }
 
-	public void setID(int lID) {
-		this.ID = ID;
-	}
+	// getters
+	public int getID() { return ID; }
+	public String getTitle() { return Title; }
+	public String getAuthor() {	return Author; }
+	public String getGenre() { return Genre; }
+	public int getCondition() {	return Condition; }
+	public boolean getAvailable() { return Available; }
 
-	public String getTitle() {
-		return Title;
-	}
-
-	public void setTitle(String title) {
-		Title = title;
-	}
-
-	public String getAuthor() {
-		return Author;
-	}
-
-	public void setAuthor(String author) {
-		Author = author;
-	}
-
-	public String getGenre() {
-		return Genre;
-	}
-
-	public void setGenre(String genre) {
-		Genre = genre;
-	}
-
-	public int getCondition() {
-		return Condition;
-	}
-
-	public void setCondition(int lCondition) {
-		this.Condition = lCondition;
-	}
-
-	public boolean isbAvailable() {
-		return bAvailable;
-	}
-
-	public void setbAvailable(boolean bAvailable) {
-		this.bAvailable = bAvailable;
-	}
-	
 }
