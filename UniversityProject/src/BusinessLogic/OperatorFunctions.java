7a62c50dde5664f0afdc8458c3f402b3d4b1d102
Daniel Kostov <45979039+DdanielRrKk@users.noreply.github.com>
2020-11-22 23:31:01 +0200

    Adding Rating Calculations and Notifications Button Function

--- UniversityProject/src/BusinessLogic/OperatorFunctions.java
+++ UniversityProject/src/BusinessLogic/OperatorFunctions.java
@@ -1,37 +1,57 @@
 package BusinessLogic;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 import DAO.AccountsDAOImplementation;
 import DAO.BooksDAOImplementation;
+import DAO.IssueDAOImplementation;
 import Model.Accounts;
 import Model.Books;
+import Model.Issue;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.concurrent.WorkerStateEvent;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ChoiceDialog;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.util.Callback;
 import tray.animations.AnimationType;
 import tray.notification.NotificationType;
 import tray.notification.TrayNotification;
 
 public class OperatorFunctions {
 	
-	private AccountsDAOImplementation adi=new AccountsDAOImplementation();
-    private BooksDAOImplementation bdi=new BooksDAOImplementation();
+	private static AccountsDAOImplementation adi=new AccountsDAOImplementation();
+    private static BooksDAOImplementation bdi=new BooksDAOImplementation();
+    private static IssueDAOImplementation idi=new IssueDAOImplementation();
 	 
     private static boolean registerFlag;
+    private static boolean issueFlag;
+    private static boolean returnedFlag;
     
-    private static boolean stopFlag=false;;
+    private static boolean stopFlag=false;
     
   //=============THREAD
     public void startNotifyThread() {
@@ -59,6 +79,32 @@ public class OperatorFunctions {
 						});
 						return null;
 					}
+					else if(issueFlag==true) {
+						Platform.runLater(new Runnable() {
+
+							@Override
+							public void run() {
+								TrayNotification tray = new TrayNotification("New Notification", "A Reader wants to borrow a book", NotificationType.INFORMATION);
+						        tray.setAnimationType(AnimationType.POPUP);
+						        tray.showAndWait();
+							}
+								
+						});
+						return null;
+					}
+					else if(returnedFlag==true) {
+						Platform.runLater(new Runnable() {
+
+							@Override
+							public void run() {
+								TrayNotification tray = new TrayNotification("New Notification", "A Reader just returned a book", NotificationType.INFORMATION);
+						        tray.setAnimationType(AnimationType.POPUP);
+						        tray.showAndWait();
+							}
+								
+						});
+						return null;
+					}
 					else {
 						Thread.sleep(2000);
 					}
@@ -73,6 +119,8 @@ public class OperatorFunctions {
 			@Override
 			public void handle(WorkerStateEvent arg0) {
 				registerFlag=false;
+				issueFlag=false;
+				returnedFlag=false;
 				
 				if(stopFlag==false) {
 					Thread thr=new Thread(notify);
@@ -97,7 +145,359 @@ public class OperatorFunctions {
 	public void registerRequest(Accounts acc) {
 		registerFlag=true;
 		
-		//adi.Insert(acc);
+		try {
+			adi.Insert(acc);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//=============ADD ISSUE REQUEST
+	public void issueRequest(Issue iss) {
+		issueFlag=true;
+			
+		try {
+			idi.Insert(iss);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+		
+	//=============RETURNED BOOK
+	public void returnedBook(Issue iss) {
+		returnedFlag=true;
+		
+		if(iss.getReturnedDate().after(iss.getReturnDate())) {
+			try {
+				Accounts acc=adi.SelectWhereID(iss.getAccountID());
+				
+				if(acc.getRatingID()!=0) {
+					acc.setRatingID(acc.getRatingID()-1);
+				}
+				
+				adi.UpdateWhereID(iss.getAccountID(), acc);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		else {
+			try {
+				Accounts acc=adi.SelectWhereID(iss.getAccountID());
+				
+				if(acc.getRatingID()!=10) {
+					acc.setRatingID(acc.getRatingID()+1);
+				}
+				
+				adi.UpdateWhereID(iss.getAccountID(), acc);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	//=============CLASS FOR THE LIST DATA
+	public static class HBoxCell extends HBox {
+        Label lbl = new Label();
+        Button btnApr = new Button();
+        Button btnRej = new Button();
+
+        HBoxCell(String type, int id) {
+             super();
+             
+             if(type.equals("Issue")) {
+            	 try {
+             		List<Issue> li=idi.SelectAll();
+     	    			    		
+      	    		for(Issue i : li) {
+      	    			if(i.getID()==id) {
+      	    				Accounts acc=adi.SelectWhereID(i.getAccountID());
+      	    				Books boo=bdi.SelectWhereID(i.getBookID());
+      	    				
+      	    				this.lbl.setText("Reader "+acc.getUsername()+" want to borrow '"+boo.getTitle()+"' book");
+      	    			}
+      	    		}                	 
+                 } catch (SQLException e) {
+                	 e.printStackTrace();
+                 }
+            	 
+            	btnApr.setText("Approve");
+   	            btnRej.setText("Reject");
+   	    		
+   	    		btnApr.setOnAction(makeApproveIssueAction(id));
+   	    		btnRej.setOnAction(makeRejectIssueAction(id));
+   	    		
+   	    		this.lbl.setMaxWidth(Double.MAX_VALUE);
+   	            HBox.setHgrow(this.lbl, Priority.ALWAYS);
+
+   	            this.getChildren().addAll(this.lbl, btnApr, btnRej);
+            	 
+             }
+             else if(type.equals("Return")) {
+            	 try {
+              		List<Issue> li=idi.SelectAll();
+      	    		
+       	    		for(Issue i : li) {
+       	    			if(i.getID()==id) {
+       	    				Accounts acc=adi.SelectWhereID(i.getAccountID());
+       	    				Books boo=bdi.SelectWhereID(i.getBookID());
+       	    				
+       	    				this.lbl.setText("Reader "+acc.getUsername()+" want to borrow '"+boo.getTitle()+"' book");
+       	    			}
+       	    		}
+       	    		
+       	    		btnApr.setOnAction(makeApproveIssueAction(id));
+                 	 
+                 } catch (SQLException e) {
+                 	 e.printStackTrace();
+                 }
+            	 
+            	 btnApr.setText("Evaluate Book");
+            	
+            	 btnApr.setOnAction(makeEvaluateAction(id));
+            	 
+            	 this.lbl.setMaxWidth(Double.MAX_VALUE);
+                 HBox.setHgrow(this.lbl, Priority.ALWAYS);
+
+                 this.getChildren().addAll(this.lbl, btnApr);
+            	 
+              }
+              else {
+            	 try {
+            		List<Accounts> la=adi.SelectAll();
+            	 
+            	 	for (Accounts a : la) {
+            	 		if(a.getID()==id) {
+      	    				this.lbl.setText("Reader "+a.getUsername()+" is waiting to be approved");
+  	    				}
+  	    			}
+            	 } catch (SQLException e) {
+                 	 e.printStackTrace();
+                 }
+            	 
+            	 btnApr.setText("Approve");
+   	           	 btnRej.setText("Reject");
+            	
+            	 btnApr.setOnAction(makeApproveAccountsAction(id));
+            	 btnRej.setOnAction(makeRejectAccountsAction(id));
+            	 
+            	 this.lbl.setMaxWidth(Double.MAX_VALUE);
+                 HBox.setHgrow(this.lbl, Priority.ALWAYS);
+
+                 this.getChildren().addAll(this.lbl, btnApr, btnRej);
+             }
+             
+        }
+	}
+	
+	
+	//====ACCOUNTS EVENTS
+	private static EventHandler<ActionEvent> makeApproveAccountsAction(int id) {
+		EventHandler<ActionEvent> ev = new EventHandler<ActionEvent>() { 
+            public void handle(ActionEvent e) { 
+            	try {
+     	    		List<Accounts> la=adi.SelectAll();
+     	    		
+     	    		for (Accounts a : la) {
+     	    			if(a.getID()==id) {
+     	    				a.setApproved(true);
+     	    				
+     	    				Accounts acc=a;
+     	    				
+         	    			adi.UpdateWhereID(acc.getID(), acc);
+     	    			}
+     	    		}
+     	    	}
+     			catch(SQLException ex) {
+     				ex.printStackTrace();
+     			}
+            } 
+        };
+        
+        return ev;
+	}
+	
+	private static EventHandler<ActionEvent> makeRejectAccountsAction(int id) {
+		EventHandler<ActionEvent> ev = new EventHandler<ActionEvent>() { 
+            public void handle(ActionEvent e) { 
+            	try {
+     	    		List<Accounts> la=adi.SelectAll();
+     	    		
+     	    		for (Accounts a : la) {
+     	    			if(a.getID()==id) {
+     	    				a.setApproved(false);
+     	    				
+     	    				Accounts acc=a;
+         	    			
+         	    			adi.UpdateWhereID(acc.getID(), acc);
+     	    			}
+     	    		}
+     	    	}
+     			catch(SQLException ex) {
+     				ex.printStackTrace();
+     			}
+            } 
+        };
+        
+        return ev;
+	}
+	
+	//=========ISSUES EVENTS
+	private static EventHandler<ActionEvent> makeApproveIssueAction(int id) {
+		EventHandler<ActionEvent> ev = new EventHandler<ActionEvent>() { 
+            public void handle(ActionEvent e) { 
+            	try {
+            		List<Issue> li=idi.SelectAll();
+     	    		   	    		
+     	    		for(Issue i : li) {
+     	    			if(i.getID()==id) {
+     	    				i.setApproved(true);
+     	    				Issue iss=i;
+         	    			idi.UpdateWhereID(iss.getID(), iss);
+         	    			
+         	    			Books boo=bdi.SelectWhereID(i.getBookID());
+         	    			boo.setAvailable(false);
+         	    			bdi.UpdateWhereID(i.getBookID(), boo);
+     	    			}
+     	    		}
+     	    		
+     	    	}
+     			catch(SQLException ex) {
+     				ex.printStackTrace();
+     			}
+            } 
+        };
+        
+        return ev;
+	}
+	
+	private static EventHandler<ActionEvent> makeRejectIssueAction(int id) {
+		EventHandler<ActionEvent> ev = new EventHandler<ActionEvent>() { 
+            public void handle(ActionEvent e) { 
+            	try {
+     	    		List<Issue> li=idi.SelectAll();
+     	    		
+     	    		for(Issue i : li) {
+     	    			if(i.getID()==id) {
+     	    				i.setApproved(true);
+     	    				
+     	    				Issue iss=i;
+         	    			
+         	    			idi.DeleteWhereID(iss.getID());
+     	    			}
+     	    		}
+     	    	}
+     			catch(SQLException ex) {
+     				ex.printStackTrace();
+     			}
+            } 
+        };
+        
+        return ev;
+	}
+	
+	//=========EVALUATE BOOK
+		private static EventHandler<ActionEvent> makeEvaluateAction(int id) {
+			EventHandler<ActionEvent> ev = new EventHandler<ActionEvent>() { 
+	            public void handle(ActionEvent e) { 
+	            	try {
+	            		List<Issue> li=idi.SelectAll();
+	     	    		
+	     	    		for(Issue i : li) {
+	     	    			Issue iss=i;
+	     	    			
+	     	    			if(iss.getID()==id) {
+	     	    				List<String> choices = new ArrayList<>();
+	     	    				choices.add("As new");
+	     	    				choices.add("Fine");
+	     	    				choices.add("Very good");
+	     	    				choices.add("Good");
+	     	    				choices.add("Fair");
+	     	    				choices.add("Worn");
+	     	    				choices.add("Poor");
+	     	    				choices.add("Very poor");
+	     	    				choices.add("For scrapping");
+
+	     	    				ChoiceDialog<String> dialog = new ChoiceDialog<>("", choices);
+	     	    				dialog.setTitle("Evaluate book condition");
+	     	    				dialog.setContentText("Choose condition:");
+	     	    				
+	     	    				Optional<String> result = dialog.showAndWait();
+	     	    				if (result.isPresent()){
+	     	    				    for(String s : choices) {
+	     	    				    	if(s.equals(result.get())) {
+	     	    				    		iss.setReturnedCondition(choices.indexOf(s));
+	     	    				    		
+	     	    				    		Books boo=bdi.SelectWhereID(iss.getBookID());
+	     	    				    		if(choices.indexOf(s)!=boo.getCondition()) {
+	     	    				    			Accounts acc=adi.SelectWhereID(iss.getAccountID());
+	     	    								
+	     	    								if(acc.getRatingID()!=0) {
+	     	    									acc.setRatingID(acc.getRatingID()-1);
+	     	    								}
+	     	    								
+	     	    								adi.UpdateWhereID(iss.getAccountID(), acc);
+	     	    				    		}
+	     	    				    		else {
+	     	    				    			Accounts acc=adi.SelectWhereID(iss.getAccountID());
+	     	    								
+	     	    								if(acc.getRatingID()!=10) {
+	     	    									acc.setRatingID(acc.getRatingID()+1);
+	     	    								}
+	     	    								
+	     	    								adi.UpdateWhereID(iss.getAccountID(), acc);
+	     	    				    		}
+	     	    				    	}
+	     	    				    }
+	     	    				}
+	     	    				
+	     	    				idi.UpdateWhereID(iss.getID(), iss);
+	     	    			}
+	     	    		}
+	     	    		
+	     	    	}
+	     			catch(SQLException ex) {
+	     				ex.printStackTrace();
+	     			}
+	            } 
+	        };
+	        
+	        return ev;
+		}
+	
+	//=========MAKE NOTIFICATIONS IN LIST
+	public ListView<HBoxCell> makeNotifications() {
+		try {
+			List<HBoxCell> list = new ArrayList<>();
+			List<Accounts> la=adi.SelectAll();
+			List<Issue> li=idi.SelectAll();
+			
+			for (Accounts a : la) {
+    			if(a.getApproved() == false) {
+    				list.add(new HBoxCell("Reader", a.getID()));
+    			}
+    		}
+			
+			for (Issue i : li) {
+    			if(i.getApproved() == false) {
+    				list.add(new HBoxCell("Issue", i.getID()));
+    			}
+    			
+    			if(i.getReturnedDate()!=null) {
+    				list.add(new HBoxCell("Return", i.getID()));
+    			}
+    		}
+	        
+	        ListView<HBoxCell> listView = new ListView<HBoxCell>();
+	        ObservableList<HBoxCell> myObservableList = FXCollections.observableList(list);
+	        listView.setItems(myObservableList);
+	        
+	        return listView;
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
     
     //========FOR ACCOUNTS
@@ -106,7 +506,9 @@ public class OperatorFunctions {
     		List<Accounts> la=adi.SelectAll();
     		
     		for (Accounts a : la) {
-    			table.getItems().add(a);
+    			if(a.getRoleID()==3) {
+    				table.getItems().add(a);
+    			}
     		}
     		
     	} catch (SQLException e) {
