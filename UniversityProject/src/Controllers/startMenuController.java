b022af16b7c55eceb22359f43eef38bae2fbd12c
Daniel Kostov <45979039+DdanielRrKk@users.noreply.github.com>
2020-12-11 15:07:02 +0200

    Start menu fully functioning

--- UniversityProject/src/Controllers/startMenuController.java
+++ UniversityProject/src/Controllers/startMenuController.java
@@ -1,12 +1,9 @@
 package Controllers;
 
+import Interfaces.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
 
 public class startMenuController {
 	
@@ -17,25 +14,12 @@ public class startMenuController {
     private Button st_toLoginBtn;
     
     @FXML
-    void st_toLogIn(ActionEvent event) {  
-    	closeStartMenuWindow();
-    	fxmlScreenLoader fcl=new fxmlScreenLoader();
-    	fcl.loadScreen("../Interfaces/logInMenu.fxml");
+    void st_toLogIn(ActionEvent event) {
+    	Main.getInstance().setScene("../Interfaces/logInMenu.fxml");
     }
 
     @FXML
     void st_toRegister(ActionEvent event) {
-    	closeStartMenuWindow();
-    	fxmlScreenLoader fcl=new fxmlScreenLoader();
-    	fcl.loadScreen("../Interfaces/registerMenu.fxml");
-    }
-    
-    //function for closing the startMenu window
-    @FXML
-    private void closeStartMenuWindow() {
-    	//by getting the window from the button, we close it
-    	//so we can open the new window without them being stacked
-    	((Stage)st_toRegisterBtn.getScene().getWindow()).close();
-    }
-    
+    	Main.getInstance().setScene("../Interfaces/registerMenu.fxml");
+    }   
 }
